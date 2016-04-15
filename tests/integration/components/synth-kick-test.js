1e0e13aeda148e3ca1bb17c8247cd68e2501f827
Joey Arnstein and Molly Curtin <joeyarnstein@gmail.com>
2016-04-15 10:16:49 -0700

    rename test file

--- tests/integration/components/synth-kick-test.js
+++ tests/integration/components/synth-kick-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('synth-kick,', 'Integration | Component | synth kick,', {
+moduleForComponent('synth-kick', 'Integration | Component | synth kick,', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{synth-kick,}}`);
+  this.render(hbs`{{synth-kick}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#synth-kick,}}
+    {{#synth-kick}}
       template block text
-    {{/synth-kick,}}
+    {{/synth-kick}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
