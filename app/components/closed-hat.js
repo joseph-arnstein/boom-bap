b06715116bb7ae99d0b6fa6ee242aab14d79dc20
chris <cehoughton71@gmail.com>
2016-04-18 09:46:25 -0700

    remove alert on closed hat

--- app/components/closed-hat.js
+++ app/components/closed-hat.js
@@ -8,7 +8,6 @@ export default Ember.Component.extend({
       sound: new Audio('new-samples/Cid_H1.wav')
     };
     console.log(testInput);
-    alert("You have selected 'Close High-Hat' and can add it to your loop below!");
     this.get('selectAudio').add(testInput);
   }
 });
