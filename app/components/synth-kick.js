0b79e8eb513fe908b4bdc0e8787c9ef607d65f43
chris <cehoughton71@gmail.com>
2016-04-18 09:54:34 -0700

    remove all alerts

--- app/components/synth-kick.js
+++ app/components/synth-kick.js
@@ -8,7 +8,6 @@ export default Ember.Component.extend({
       sound: new Audio('new-samples/Dope Kicks (24).wav')
     };
     console.log(testInput);
-    alert("You have selected 'Synth-Kick' and can add it to your loop below!");
     this.get('selectAudio').add(testInput);
   }
 });
