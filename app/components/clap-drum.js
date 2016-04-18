d246d8c2c4cb498ea01c0da07226753c27f91f43
chris <cehoughton71@gmail.com>
2016-04-18 09:45:10 -0700

    remove alert on clap-drum

--- app/components/clap-drum.js
+++ app/components/clap-drum.js
@@ -8,7 +8,6 @@ export default Ember.Component.extend({
       sound: new Audio('new-samples/02_03_claps.wav')
     };
     console.log(testInput);
-    alert("You have selected 'Clap' and can add it to your loop below!");
     this.get('selectAudio').add(testInput);
   }
 });
