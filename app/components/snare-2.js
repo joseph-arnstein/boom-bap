2d23b89faefb8916eea7bef78d25687bcd01e3a1
chris <cehoughton71@gmail.com>
2016-04-18 09:52:51 -0700

    remove alert on snare-2

--- app/components/snare-2.js
+++ app/components/snare-2.js
@@ -8,7 +8,6 @@ export default Ember.Component.extend({
       sound: new Audio('new-samples/12_13_snare.wav')
     };
     console.log(testInput);
-    alert("You have selected 'Snare 2' and can add it to your loop below!");
     this.get('selectAudio').add(testInput);
   }
 });
