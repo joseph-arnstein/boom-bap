f3fb42ecde812ce2b338c6b4a3f046a933561c36
chris <cehoughton71@gmail.com>
2016-04-18 09:43:28 -0700

    remove alert on clap2

--- app/components/clap-2.js
+++ app/components/clap-2.js
@@ -8,7 +8,6 @@ export default Ember.Component.extend({
       sound: new Audio('new-samples/Clap (2).wav')
     };
     console.log(testInput);
-    alert("You have selected 'Clap 2' and can add it to your loop below!");
     this.get('selectAudio').add(testInput);
   }
 });
