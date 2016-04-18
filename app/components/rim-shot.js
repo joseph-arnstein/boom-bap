94d6755d9cf4b15eab8b4990201e3a2d5448d989
chris <cehoughton71@gmail.com>
2016-04-18 09:50:17 -0700

    remove alert on rim-shot

--- app/components/rim-shot.js
+++ app/components/rim-shot.js
@@ -8,7 +8,6 @@ export default Ember.Component.extend({
       sound: new Audio('new-samples/09_05_rim.wav')
     };
     console.log(testInput);
-    alert("You have selected 'Rim Shot' and can add it to your loop below!");
     this.get('selectAudio').add(testInput);
   }
 });
